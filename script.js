2e5ac3f813cd7e11bb9f52b8bfe8216fc4d48826
karan kumar baitha <116413125+karankumar544@users.noreply.github.com>
2023-09-25 13:00:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,13 @@ function translateToHindi() {
     const inputText = document.getElementById('input-text').value;
     const outputText = document.getElementById('output-text');
 
+    if(inputText === "")
+    {
+        alert("please inter your text")
+    }
+    else{
         outputText.textContent = inputText + "( in hindi )";
-}
+    }
 
 // Attach the translation function to the button click event
 const translateButton = document.getElementById('translate-btn');
